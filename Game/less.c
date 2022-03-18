ab0b3767c26e0a5f657dced4e31a91f0d54ab0b0
aaristegui <39808834+aristewi@users.noreply.github.com>
2022-03-18 10:55:11 +0100

    Create less.c

---
+++ Game/less.c
@@ -0,0 +1,29 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+int main(int argc, char *argv[]){
+  
+  if (argc!=2){
+    printf("Pick a different item to less");
+    printf("\n");
+    return 0;
+    
+  }else{
+
+      char buffer[50];
+      char c;
+      int file;
+      file = open(argv[1], O_RDWR);
+
+        if (file != -1){
+          read(file, buffer, 50);
+          printf("\n %s \n", buffer);
+        }else{
+            printf("There is no %s here.\n", argv[1]);
+        }
+    close(file);
+    return 0;
+  }
+
+    return 0;
+}
