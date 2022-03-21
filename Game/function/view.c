5c2ae76cad777e7509e4839709d735c9e0cd3216
esteban arroyo <esteban71150@gmail.com>
2022-03-21 10:29:55 +0100

    Update function view
    You can now see the  next room but you can't see the next next room because you aren't superman.

--- Game/function/view.c
+++ Game/function/view.c
@@ -12,7 +12,7 @@
 
 #include "view.h"
 
-
+int verif(char argv[]);
 
 void view(int argc,char *argv[])
 {
@@ -56,6 +56,11 @@ void view(int argc,char *argv[])
 			}
 			else
 			{
+
+					
+					if(verif(argv[1]))
+					{
+					
 					
 					DIR *d =opendir(argv[1]);
 					if(d)
@@ -73,14 +78,131 @@ void view(int argc,char *argv[])
 					}
 					else
 					{
+						
 						write(1,"this room doesn't exist\n", strlen("this room doesn't exist\n"));
+					}
 					}
 					
 			}
 			break;
-			
+		case 3:;
+			 if(verif(argv[1]) && verif(argv[2]))
+                {
+					if(strcmp(argv[1],"-r")==0 || strcmp(argv[1],"-room")==0)
+					{					
+										
+										
+										DIR *d =opendir(argv[2]);
+                                        if(d)
+                                        {
+												chdir(argv[2]);
+                                                while((dir =readdir(d)) !=NULL)
+                                                {
+                                                        stat(dir->d_name,buf);
+														if (S_ISDIR(buf->st_mode))
+														{
+																															
+																if (dir->d_name[0] != '.' && dir->d_name[strlen(dir->d_name)-1] != '~')
+																{
+																	write(0,dir->d_name, strlen(dir->d_name));
+																	write(0,"\n", strlen("\n"));
+																}
+															
+					
+														}
+												}
+												chdir("..");
+                                                closedir(d);
+												
+                                        }
+                                        else
+                                        {
+
+                                                write(1,"this room doesn't exist\n", strlen("this room doesn't exist\n"));
+                                        }
+					}
+					else if (strcmp(argv[2],"-r")==0 || strcmp(argv[2],"-room")==0)
+					{
+										
+										DIR *d =opendir(argv[1]);
+                                        if(d)
+                                        {
+												chdir(argv[1]);
+                                                while((dir =readdir(d)) !=NULL)
+                                                {
+                                                        stat(dir->d_name,buf);
+														if(S_ISREG(buf->st_mode))
+														{
+															break;
+														}
+														else if (S_ISDIR(buf->st_mode))
+														{
+															
+																if (dir->d_name[0] != '.' && dir->d_name[strlen(dir->d_name)-1] != '~')
+																{
+																	write(0,dir->d_name, strlen(dir->d_name));
+																	write(0,"\n", strlen("\n"));
+																}
+															
+					
+														}
+												}
+												chdir("..");
+                                                closedir(d);
+												
+                                        }
+                                        else
+                                        {
+                                                write(1,"this room doesn't exist\n", strlen("this room doesn't exist\n"));
+                                        }
+					}
+					else
+					{
+						DIR *d =opendir(argv[1]);
+						if(d)
+						{
+							while((dir =readdir(d)) !=NULL)
+							{
+								if (dir->d_name[0] != '.' && dir->d_name[strlen(dir->d_name)-1] != '~')
+								{
+									write(0,dir->d_name, strlen(dir->d_name));
+									write(0,"\n", strlen("\n"));
+					
+								}		
+							}
+							closedir(d);
+						}
+						else
+						{
+						
+							write(1,"this room doesn't exist\n", strlen("this room doesn't exist\n"));
+						}
+					}
+					
+     
+				}
+				break;
 	}
 	free(buf);
 	
 }
+
+
+int verif(char argv[])
+{
+	char test[2]="/.";
+	for(size_t i=0;i<strlen(argv);i++)
+	{
+		for(size_t t=0;t<strlen(test);t++)
+		{
+			if(argv[i]==test[t])
+			{
+				write(1,"You can't use this atribut.\n\n", strlen("You can't use this atribut\n\n"));
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
+
 
