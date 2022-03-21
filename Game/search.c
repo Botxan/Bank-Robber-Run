bd70a25d559b18da82db8e236d8ec2f6145a5bff
Kustonm <65606861+Kustonm@users.noreply.github.com>
2022-03-21 10:13:01 +0100

    Complementary function for use
    Return the byte where a ceratin word is located

--- Game/search.c
+++ Game/search.c
@@ -1,12 +1,12 @@
 int search(char *word , int file){
    char line[4] ;
-   int i = 0;
+   int i = 4;
 
-   while(read(file,line[i],3) == 1){
+   while(read(file,line[i],4) == 1){
     if(strcmp(line, word)){
      return i;
     }
    memset(line,0,strlen(line));
-   i = i+3;
+   i = i+4;
    }
 }
