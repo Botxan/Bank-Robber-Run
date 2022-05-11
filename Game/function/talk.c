196ab293a58a9522735630de42de2b5e250c5405
Andon22EHU <andonivelasco22@gmail.com>
2022-05-11 11:30:14 +0200

    Talk function fully operational

--- Game/function/talk.c
+++ Game/function/talk.c
@@ -42,21 +42,20 @@ int searchTalk(char *word , int file){
 	char text[200];
 	int readChars=0;
 	char continues;
+	char state[2];
 
 
 #ifdef FUNCTION
 int talk(char* npc){
 #else
 int main(int argc,char* argv[]) {
-	printf("Hello\n");
 
-	if (argc<1 && argc>5)return -1;
-	char *npc=argv[0];
+	if (argc<1&&strcmp(argv[0],"talk")!=0) return -1;
+	char *npc=argv[1];
 #endif
 	end=0;
 	var='A';
 	readChars=0;
-	continues='1';
 	current=getcwd(NULL,200);
 	strcat(current,"/");
 	strcat(current,npc);
@@ -65,16 +64,15 @@ int main(int argc,char* argv[]) {
 	int fd=open(current,O_RDWR);
 	if(fd==-1) return 1;
 	lseek(fd,0,SEEK_SET);
-	if(i!=0){
-		write(0,"\nWe have nothing else to talk about\n",strlen("\nWe have nothing else to talk about\n"));
-		return 0;
-	}
-	while(continues=='1'){
+	read(fd,&state[0],1);
+	continues='y';
+	state[1]='#';
+	searchTalk(state,fd);
+	while(continues=='y'){
 		write(0,npc,strlen(npc));
 		write(0,":\n",strlen(":\n"));
             	branch[0]=i+'0';
 		branch[1]=var;
-		lseek(fd,0,SEEK_SET);
 		searchTalk(branch,fd);
 		end=searchTalk("--",fd)-4;
 		read(fd,&continues,1);
@@ -82,16 +80,19 @@ int main(int argc,char* argv[]) {
 		read(fd,&text,end);
 		write(0,text,end);
  		text[0]='\0';
-		if(continues=='1'){
+		if(continues=='y'){
 			write(0,"\n-----Choose one-----\n",strlen("\n-----Choose one-----\n"));
 			readChars=scanf(" %[^\n]%*c", &var);
-		}
-		while(readChars!=1){
-			write(0,"\nThere was an error, please choose an option again\n",strlen("\nThere was an error, please choose an option again\n"));
-               		readChars=scanf(" %[^\n]%*c", &var);
+		
+			while(readChars!=1){
+				write(0,"\nThere was an error, please choose an option again\n",strlen("\nThere was an error, please choose an option again\n"));
+               			readChars=scanf(" %[^\n]%*c", &var);
+			}
 		}
 		i++;
 	}
+lseek(fd,0,SEEK_SET);
+write(fd,&continues,1);
 write(0,"\n",strlen("\n"));
 return 0;
 }
