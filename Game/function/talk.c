cff284e0f74143bb200a9d37988733e732ed9edc
Andon22EHU <andonivelasco22@gmail.com>
2022-05-12 13:05:51 +0200

    talk finished and robert convo

--- Game/function/talk.c
+++ Game/function/talk.c
@@ -39,11 +39,12 @@ int searchTalk(char *word , int file){
 	char var='A';
 	char branch[2]="  ";
 	int end=0;
-	char text[200];
+	char text[4096];
 	int readChars=0;
 	char continues;
 	char state[2];
-
+	char initialState;
+	int error;
 
 #ifdef FUNCTION
 int talk(char* npc){
@@ -55,6 +56,7 @@ int main(int argc,char* argv[]) {
 #endif
 	end=0;
 	var='A';
+	error=0;
 	readChars=0;
 	current=getcwd(NULL,200);
 	strcat(current,"/");
@@ -67,32 +69,39 @@ int main(int argc,char* argv[]) {
 	read(fd,&state[0],1);
 	continues='y';
 	state[1]='#';
+	initialState=state[0];
 	searchTalk(state,fd);
-	while(continues=='y'){
+	while(continues=='y'&&error==0){
 		write(0,npc,strlen(npc));
 		write(0,":\n",strlen(":\n"));
             	branch[0]=i+'0';
 		branch[1]=var;
 		searchTalk(branch,fd);
 		end=searchTalk("--",fd)-4;
-		read(fd,&continues,1);
-		lseek(fd,-end-3,SEEK_CUR);
-		read(fd,&text,end);
-		write(0,text,end);
- 		text[0]='\0';
-		if(continues=='y'){
-			write(0,"\n-----Choose one-----\n",strlen("\n-----Choose one-----\n"));
-			readChars=scanf(" %[^\n]%*c", &var);
+		if(end>0){
+			read(fd,&continues,1);
+			lseek(fd,-end-3,SEEK_CUR);
+			read(fd,&text,end);
+			write(0,text,end);
+ 			text[0]='\0';
+			if(continues=='y'){
+				write(0,"\n-----Choose one-----\n",strlen("\n-----Choose one-----\n"));
+				readChars=scanf(" %[^\n]%*c", &var);
 		
-			while(readChars!=1){
-				write(0,"\nThere was an error, please choose an option again\n",strlen("\nThere was an error, please choose an option again\n"));
-               			readChars=scanf(" %[^\n]%*c", &var);
+				while(readChars!=1){
+					write(0,"\nThere was an error, please choose an option again\n",strlen("\nThere was an error, please choose an option again\n"));
+               				readChars=scanf(" %[^\n]%*c", &var);
+				}
 			}
-		}
-		i++;
+			i++;
+		} else{ 
+			error=1;
+			printf("The answer given wasn't an option restart conversation");
+			}
 	}
 lseek(fd,0,SEEK_SET);
-write(fd,&continues,1);
+if(error==0) write(fd,&continues,1);
+else write(fd,&initialState,1);
 write(0,"\n",strlen("\n"));
 return 0;
 }
