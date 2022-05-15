67b87afabfdd6298400b90e55363a67557919610
Kustonm <65606861+Kustonm@users.noreply.github.com>
2022-05-16 01:26:09 +0200

    Update StoreMoves.c
    Minor changes

--- Game/function/StoreMoves.c
+++ Game/function/StoreMoves.c
@@ -1,3 +1,5 @@
+  GNU nano 2.3.1                                                                       File: StoreMoves.c                                                                                                                                                    
+
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <stdio.h>
@@ -14,30 +16,33 @@ int main (int argc, char* argv[])
  int line =0;
  int bs = 0;
  int i = 0;
- char buffC[20];char *cmd;char* token;
+ char buffC[20];char buffIn[20];char *cmd;char* token;
  int fd =  open("moves.txt",O_RDWR|O_CREAT,0666);
 
-while(1)
-{
 
  lseek(fd,0,SEEK_END);
  memset(buffC,0,20);
-
- while(read(STDIN_FILENO,buffC,20)>0)
+ //printf("BATATA\n");
+ while(1)
  {
-    token = strtok(buffC," ");
+  while(read(STDIN_FILENO,buffC,20)>0)
+  {
+     token = strtok(buffC," ");
 
-    while(token != NULL)
-    {
-     write(fd,token,strlen(token));
-     token = strtok(NULL," ");
-     if(token != NULL)
+     while(token != NULL)
      {
-      write(fd," ",1);
+      write(fd,token,strlen(token));
+      write(fd,"\n",1);
+      //write(STDOUT_FILENO, token, strlen(token));
+      token = strtok(NULL," ");
+      if(token != NULL)
+      {
+       write(fd," ",1);
+      }
      }
-    }
-    break;
+   break;
   }
+ }
 }
-return 0;
-}
+
+
