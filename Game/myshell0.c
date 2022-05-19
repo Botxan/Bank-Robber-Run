da3de961f780a193c5461b8d0650b7a4c2153db0
Kustonm <65606861+Kustonm@users.noreply.github.com>
2022-05-20 00:41:05 +0200

    H

--- Game/myshell0.c
+++ Game/myshell0.c
@@ -629,7 +629,7 @@ int execute(int argc, char *argv[])
 					break;
 				case VAULTC:
 					int mls = 2;int mam = 7;int i = 0; int o = 0; int b = 0;
-					char ans[mam][mls] ={"up","up","dw","rg","lf","rg","dw"}; char mIn[mam][mls];char mOut[mam][mls];char player[mls];
+					char *ans[] ={"up","up","dw","rg","lf","rg","dw"}; char mIn[mam][mls];char mOut[mam][mls];char player[mls];
 					if(visitedTimes%2 != 0){
                                          printf("*It seens to be some kind of laser... I should remember my moves* ");
 					 while(i < mam)
